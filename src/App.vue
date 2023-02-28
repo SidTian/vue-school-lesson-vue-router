ea8ab92c6213ef6c24508268fdbd7234cf9a2150
田家岩 <844015489@qq.com>
2023-02-28 10:44:58 +0800

    chapter 1 finish

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <router-view></router-view>
+    <router-link to="/">
+      <button>Home</button>
+    </router-link>
+    <router-link to="/about">
+      <button>About</button>
+    </router-link>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="About" class="about">
+    <h1>About</h1>
+  </div>
+</template>
